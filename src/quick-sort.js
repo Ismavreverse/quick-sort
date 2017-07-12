3f019fd13777de1aabf6ad3473422894a37006d9
Vamsi Kalidindi <Vamsi_Kalidindi@epam.com>
2017-07-12 15:37:27 +0530

    quick sort implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,22 @@
 'use strict';
-
+var input=[9, 8, 7, 10, 6, 100, 5, 4, 99, 3, 2, 1];
 function quickSort(input) {
-
-};
+	if (input.length <= 1) { 
+		return input;
+	} else {
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = input.pop();
+		var length = input.length;
+		for (var i = 0; i < length; i++) {
+			if (input[i] <= pivot) {
+				left.push(input[i]);
+			} else {
+				right.push(input[i]);
+			}
+		}
+		return newArray.concat(quickSort(left), pivot, quickSort(right));
+	}
+}
 module.exports = quickSort;
